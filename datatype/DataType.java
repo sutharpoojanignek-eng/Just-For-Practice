63fe9756d209249a281f80ef8d03b2030f9683de
Poojan Suthar <suthar.poojan.ignek@gmail.com>
2026-02-04 14:06:18 +0530

    Adding Datatype File

---
+++ datatype/DataType.java
@@ -0,0 +1,47 @@
+package datatype;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		//Primitive Data-Types
+		//Byte  Range: -128 to 127
+		byte myNum = 100;
+		
+		//short Range:-32,768 to 32,767
+		short mediumNumber = 32000;
+		
+		//int Range: -2,147,483,648 to 2,147,483,647
+		int age = 25;
+		
+		//Long Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		long largeNumber = 10000000000L;
+		
+		//Float Range: Approximately ±3.40282347E+38F (6–7 significant decimal digits)
+		float height = 5.75f;
+		
+		//Double Range: Approximately ±1.79769313486231570E+308 (15 significant decimal digits)
+		double distance = 12345.6789;
+		
+		//Character Range: ‘\u0000’ (or 0) to ‘\uffff’ (or 65,535 inclusive)
+		char letter = 'A';
+		
+		//Boolean Values: true or false
+		boolean isJavaFun = true;
+		
+		
+		//Non-Primitive Data-Types
+		//String
+		String name  = "Poojan";
+		
+		//Array
+		int[] anArray = new int[10];
+		int[] array = {12,34,25};
+
+		//Class
+		
+		//Interface
+		
+	}
+
+}
